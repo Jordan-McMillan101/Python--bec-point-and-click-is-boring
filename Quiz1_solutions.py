67a07e84e346cfdadce7451a391ac149b7af987b
Jordan McMillan <mcmillan.101@buckeyemail.osu.edu>
2015-10-25 21:55:55 -0400

    Update Quiz1_solutions.py
    Added a few more functions

--- Quiz1_solutions.py
+++ Quiz1_solutions.py
@@ -46,12 +46,62 @@ print (hasSuffix(w, suff))
 # returns the n-th largest number
 def nTh_LargestNum(nums, num):
     nums.sort()
-    for i in range(1,len(nums)):
-        if nums[i] == num:
-            print ('The ' + str(num)+'th largest number in the list is: ' +str(nums[i]))
+    print ('The ' + str(num)+' largest number in the list is: ' +str(nums[len(nums)-num]))
 
-
+#test
 nums1 = [1,5,10,15,2,4,1]
-num1 = 3
+num1 = 2 #nth largest number in this case the 2nd largest which is 10
 print('list of numbers: ' + str(nums1))
-nth_LargestNum(nums1, num1)
+nTh_LargestNum(nums1, num1)
+
+"""
+#Question 10.) (currently not working and im not sure why)
+# Given a list this will return the standard deviation of the list.
+import math
+def standardDeviation(nums):
+    mean = 0
+    summation = 0
+    for i in range(0,len(nums)):#calculate mean
+        mean = mean+nums[i]
+    for j in range(0, len(nums)):#calculate sqrt(nums[i]-mean)
+        summation = summation + (math.pow((nums[j]-mean),2)
+    sd = math.sqrt((summation)/(len(nums)) #summation of all numbers
+    return sd
+
+
+#test
+list1 = [1,2,4,5,6,7,8,9,10]
+print (str(standardDeviation(list1)))
+"""
+"""
+#Question 11.) 
+
+def deg2dms(decimalDeg):
+    #deg1 = deg
+    d = int(decimalDeg)
+    print(str(d) + ' degrees')
+    minutes = int(((decimalDeg-d) * 60))
+    print(str(minutes)+' minutes')
+    seconds = (decimalDeg-d-(minutes/60))*3600
+    print(str(seconds)+ ' seconds')
+
+#main
+deg1 = 30.263888889
+print deg1
+deg2dms(deg1)
+
+"""
+
+#Quesiton 12.)
+#Given an integer this function will print x number of rows of lists where the end two numbers in each
+#row are added together
+def tri(num):
+    for i in range(0,num):
+
+
+
+#test
+num2 = 8
+tri(8)
+
+
